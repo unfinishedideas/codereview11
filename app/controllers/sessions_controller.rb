bbaf7d920ad9d20cc61d08ecc8be15901fb1950f
Peter Wells <petethebeatwells@gmail.com>
2020-01-24 14:44:07 -0800

    troubleshooting redirect for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     if @user
       flash[:notice] = "You've signed in."
       session[:user_id] = @user.id
-      redirect_to "/"
+      redirect_back(fallback_location: root_path)
     else
       flash[:alert] = "There was a problem signing in. Please try again."
       redirect_to signin_path
@@ -15,6 +15,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:notice] = "You've signed out."
-    redirect_to "/"
+    redirect_back(fallback_location: root_path)
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    store_location
   end
 
   def create
