3c01b81cf1141cf2606bf402f983ff1a14917d16
Peter Wells <petethebeatwells@gmail.com>
2020-01-24 17:07:15 -0800

    get add product specs passing

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 
 describe "the add a product process" do
-  it "adds a new product" do
-    # visit products_path
+
+  before(:each) do
+    User.create!(:email => 'admin@admin.com', :password => '123', :password_confirmation => '123', :admin => true, :username => 'Admin Man')
+  end
+
+  def get_admin
     visit '/'
-    # click_link 'Sign up'
-    # fill_in 'Email', :with => 'admin@admin.com'
-    save_and_open_page
     click_link 'Sign in'
     fill_in 'Email', :with => 'admin@admin.com'
     fill_in 'Password', :with => '123'
-    click_on 'Sign in'
+    click_on 'Log in'
+  end
+
+  it "adds a new product" do
+    visit '/'
+    get_admin
+    visit products_path
     click_link 'Create new product'
     fill_in 'Name', :with => 'The Juicinator 3000'
     fill_in 'Cost', :with => 350
@@ -21,6 +28,7 @@ describe "the add a product process" do
   end
 
   it "gives an error when no name is entered" do
+    get_admin
     visit new_product_path
     click_on 'Create Product'
     expect(page).to have_content "Name can't be blank"
--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -1,28 +1,42 @@
 require 'rails_helper'
 
 describe "the add a review process" do
+
+  before(:each) do
+    User.create!(:email => 'admin@admin.com', :password => '123', :password_confirmation => '123', :admin => true, :username => 'Admin Man')
+  end
+
+  def get_admin
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'Email', :with => 'admin@admin.com'
+    fill_in 'Password', :with => '123'
+    click_on 'Log in'
+  end
+
   it "adds a review to a product" do
     project = Product.create({name: "dope thing", cost: 5, country_of_origin: "USA" })
+    get_admin
     visit products_path
     click_link 'Dope Thing'
     click_link 'Review this product'
-    fill_in 'Author', :with => 'Sandy Sally'
     fill_in 'Rating', :with => 5
-    fill_in 'Content body', :with => "USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA"
+    fill_in 'review_content_body', :with => "USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA"
     click_on 'Create Review'
     expect(page).to have_content 'Review successfully added!'
-    expect(page).to have_content('Sandy Sally')
+    expect(page).to have_content('USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA')
   end
 
   it "gives an error when no name is entered" do
     project = Product.create({name: "dope thing", cost: 5, country_of_origin: "USA" })
+    get_admin
     visit products_path
     click_link 'Dope Thing'
     click_link 'Review this product'
-    fill_in 'Author', :with => ''
     fill_in 'Rating', :with => 5
-    fill_in 'Content body', :with => "USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA USA"
+    fill_in 'review_content_body', :with => ""
     click_on 'Create Review'
+    save_and_open_page
     expect(page).to have_content "Author can't be blank"
   end
 end
